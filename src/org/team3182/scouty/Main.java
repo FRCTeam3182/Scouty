7ea926bedd862f9742feadf804d79825d1a30a11
nodcah <nodcah@gmail.com>
2015-03-28 15:05:55 -0400

    Add Parse library

--- src/org/team3182/scouty/Main.java
+++ src/org/team3182/scouty/Main.java
@@ -2,6 +2,7 @@ package org.team3182.scouty;
 
 import android.app.Activity;
 import android.os.Bundle;
+import com.parse.Parse;
 
 public class Main extends Activity {
     /**
@@ -11,5 +12,12 @@ public class Main extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        // Enable Local Datastore.
+        Parse.enableLocalDatastore(this);
+
+        Parse.initialize(this, "ePfmPvijcOTPfbKwcz6Ap7uAPWICrmi6rR6pyJ6T", "proSRvcvzD7Hs1KXCdp2qz5Xhiae3563a5NUUNxe");
+
+        
     }
 }
