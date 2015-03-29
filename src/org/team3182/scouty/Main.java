f016cf72ca6380ecb0eea920ffe3ff3b8aeb18a4
Eric Zeiberg <ejzeiberg@gmail.com>
2015-03-28 20:34:27 -0400

    Setup tab view

--- src/org/team3182/scouty/Main.java
+++ src/org/team3182/scouty/Main.java
@@ -2,9 +2,16 @@ package org.team3182.scouty;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import com.allthelucky.common.view.ViewPagerIndicatorView;
 import com.parse.Parse;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Main extends Activity {
+    
     /**
      * Called when the activity is first created.
      */
@@ -17,6 +24,12 @@ public class Main extends Activity {
         Parse.enableLocalDatastore(this);
 
         Parse.initialize(this, getString(R.string.appID), getString(R.string.clientKey));
+
+        ViewPagerIndicatorView viewPagerIndicatorView = (ViewPagerIndicatorView) findViewById(R.id.base_view);
+        final Map<String, View> map = new HashMap<String, View>();
+        map.put("Tab 1", LayoutInflater.from(this).inflate(R.layout.activity_sample_pager_0, null));
+        map.put("Tab 2", LayoutInflater.from(this).inflate(R.layout.activity_sample_pager_1, null));
+        viewPagerIndicatorView.setupLayout(map);
 
         
     }
